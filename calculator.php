f06237f5a67d2762e6cf0333da629b739a0a009a
Artyom <545436@edu.rocmn.nl>
2021-09-29 11:39:59 +0200

    calc kanker

--- calculator.php
+++ calculator.php
@@ -17,10 +17,31 @@ $bedrag = $_REQUEST['itemPrice'];
 
 
 
-function calculate($btwCodes , $amount , $bedrag){
-
-    $result = $bedrag 
+function calculate($btwCodes , $amount , $bedrag , $btwCode){
+    var_dump($btwCode, $amount, $bedrag);
+    $total = $bedrag * $amount;
+    if($amount < 1000){
+        $discount = 0;
+    }
+    
+    if($amount >= 1000 && $amount < 6000){
+        $discount = 3;
+    }elseif($amount >=5000 && $amount < 7000){
+        $discount = 5;
+    }elseif($amount >=7000 && $amount < 10000 ){
+        $discount = 7;
+    }elseif($amount >=10000 && $amount <50000){
+        $discount = 10;
+    }
+    
+    $valueDiscount = $total/100 * $discount;
+    $total = $total - $discount;
+    $incBTW = $total/100 * $btwCodes[$btwCode];
+    var_dump($incBTW);
+    echo $incBTW;
 }
+calculate($btwCodes, $amount, $bedrag, $btwCode);
+
 
 
 ?>
